35ebccf23775b7f009038cb71410c608b4907ee4
Nguyen Anh Ngoc Minh <22010476@st.phenikaa-uni.edu.vn>
2024-09-26 18:01:04 +0700

    upd

---
+++ global-sanctuary-fund-challenge-master/__tests__/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const navElement=document.getElementsByTagName('nav');
+    // expect(navElement.length).toEqual(11);
+    // const footerElement=document.getElementsByTagName('footer');
+    // expect(footerElement.length).toEqual(11);
